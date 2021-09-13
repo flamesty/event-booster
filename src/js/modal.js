120281fe9eefd1e9ea096bf2898b001ee1014dde
Iuliia-Shchur <79912506+Iuliia-Shchur@users.noreply.github.com>
2021-09-13 17:37:51 +0300

    modal/features/open
    добавлен js на открытие, закрытие, сделан hbs

---
+++ src/js/modal.js
@@ -0,0 +1,53 @@
+import { eventsApiService } from "./api-event-service.js";
+import { renderService } from "./search-render-service.js";
+import modalMarkup from '../templates/modal-markup.hbs';
+
+
+const refs = {
+modal: document.querySelector('.modal'),
+modalContainer: document.querySelector('.modal__content'),
+button: document.querySelector('.modal__button'),
+overlay: document.querySelector('.backdrop'),
+eventsList: document.querySelector('.events-list'),
+lightbox: document.querySelector('.js-lightbox'),
+}
+
+refs.eventsList.addEventListener('click', modalIsOpen);
+refs.button.addEventListener('click', onCloseModalBtn);
+refs.overlay.addEventListener('click', onCloseModalOverlay);
+window.addEventListener('keyup', onCloseModalEsc);
+
+function modalIsOpen (e) {
+   
+refs.overlay.classList.add('is-open');
+refs.overlay.classList.remove('is-hidden');
+};
+
+function onCloseModalBtn (e) {
+    
+      removeClassIsOpen();
+    
+      };
+
+
+function onCloseModalOverlay (e) {
+    if (e.currentTarget === e.target) {
+      removeClassIsOpen();
+  }
+  };
+
+
+function onCloseModalEsc (e) {
+    if (e.key !== "Escape") {
+      return;
+    }
+    removeClassIsOpen();
+  };
+  
+  function removeClassIsOpen () {
+    refs.overlay.classList.remove('is-open');
+    refs.overlay.classList.add('is-hidden');
+    
+  };
+
+
