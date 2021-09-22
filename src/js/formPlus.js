f2ddfffe5f53c21aa9be4034bed040f6211bdfdf
Oleh-Mohylnyi <oleh.mohylnyi@gmail.com>
2021-09-22 13:32:11 +0300

    заменяет галочку бесконечного скролла

--- src/js/formPlus.js
+++ src/js/formPlus.js
@@ -1,9 +1,32 @@
+
+import { refsGen } from '../js/refs';
+
 const refBtnOpenMenu = document.querySelector('.checkbox-open-menu')
-const refFormSort = document.querySelector('.form-sort')
+const refFormSort = document.querySelector('.form')
+const refSelectSort = document.querySelector('.select-sort')
+const refSelectScroll = document.querySelector('.select-scroll')
 
 refBtnOpenMenu.addEventListener("click", openMenu);
+refSelectSort.addEventListener("change", changeValueSort);
+refSelectScroll.addEventListener("change", changeValueScroll);
 
 function openMenu() {
     refFormSort.classList.toggle("hidden-visually")
 
+}
+
+
+function changeValueSort(e) {
+    let n = refSelectSort.options.selectedIndex
+    refsGen.sortType = refSelectSort.options[n].value
+}
+
+function changeValueScroll(e) {
+    let n = refSelectScroll.options.selectedIndex
+    if (refSelectScroll.options[n].value === "true") {
+        refsGen.UNLESS_SCROLL = true
+    } else {
+        refsGen.UNLESS_SCROLL = false
+    }
+    console.log(refsGen.UNLESS_SCROLL)
 }
