df3ae6998519aec09abd3372fc4f27c16a147ebb
Oleh-Mohylnyi <oleh.mohylnyi@gmail.com>
2021-09-22 11:06:36 +0300

    добавляет меню

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { refsGen } from './js/refs';
 import { renderService } from './js/search-render-service';
 import { toTop } from './js/to-top-btn';
 import './js/notify.js';
+import './js/formPlus.js';
 
 toTop.createBtnAndEvnListeners();
 window.addEventListener('DOMContentLoaded', () => renderService.initialAtStartup(refsGen));
